17abf2ca29412ae70a779d195c058fd861fe55b6
kris-marthy1 <120700722+kris-marthy1@users.noreply.github.com>
2025-03-18 09:05:25 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 export const metadata = {
   title: "QueueTrack - An Automated Queue Management and Tracking System |  Online Queue System", // Optimized title
   description: "Experience an efficient and real-time queue management system designed to prioritize customers and reduce wait times.",
+   icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
